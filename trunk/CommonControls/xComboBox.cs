f3c5da52219f4535457cab6ecf1bc0ae37733ee5
vinsonhwj <vinsonhwj@e2a3c97a-9f89-11dd-8937-a1f7b47bb54a>
2009-12-16 10:39:32 +0000

    ComboBox增加IsRequired属性

--- trunk/CommonControls/xComboBox.cs
+++ trunk/CommonControls/xComboBox.cs
@@ -17,18 +17,42 @@ namespace hwj.UserControls.CommonControls
         /// </summary>
         [DefaultValue(null), Description("设置引发hwj.UserControls.ValueChanged事件的对象"), Browsable(false)]
         protected Function.Verify.ValueChangedHandle ValueChangedHandle { get; set; }
+        protected Function.Verify.RequiredHandle RequiredHandle { get; set; }
+
+        private bool _IsRequired = false;
+        /// <summary>
+        /// 获取或设置为必填控件
+        /// </summary>
+        [DefaultValue(false), Description("获取或设置为必填控件")]
+        public bool IsRequired
+        {
+            get { return _IsRequired; }
+            set
+            {
+                _IsRequired = value;
+                SetRequiredStatus();
+            }
+        }
+
+        [Browsable(false)]
+        public System.Drawing.Color OldBackColor { get; set; }
         #endregion
 
         public xComboBox()
         {
             EnterEqualTab = true;
             ValueChangedHandle = Common.ValueChanged;
+            OldBackColor = this.BackColor;
+            IsRequired = false;
+            OldBackColor = this.BackColor;
         }
 
         #region Override Function
         protected override void OnCreateControl()
         {
             base.OnCreateControl();
+            RequiredHandle = Common.Required;
+            SetRequiredStatus();
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
@@ -48,7 +72,35 @@ namespace hwj.UserControls.CommonControls
             if (this.Focused && ValueChangedHandle != null)
                 ValueChangedHandle.IsChanged = true;
             base.OnTextChanged(e);
+            SetRequiredStatus();
+        }
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            base.OnEnabledChanged(e);
+            SetRequiredStatus();
         }
         #endregion
+
+        public void SetRequiredStatus()
+        {
+            if (DesignMode) return;
+
+            if (IsRequired && string.IsNullOrEmpty(this.Text) && Enabled)
+            {
+                if (RequiredHandle != null)
+                {
+                    RequiredHandle.Add(this);
+                }
+                this.BackColor = Common.RequiredBackColor;
+            }
+            else
+            {
+                if (RequiredHandle != null)
+                {
+                    RequiredHandle.Remove(this);
+                }
+                this.BackColor = this.OldBackColor;
+            }
+        }
     }
 }
