457657489b829207c97ef56078a92a6b57f8490f
vinsonhwj <vinsonhwj@e2a3c97a-9f89-11dd-8937-a1f7b47bb54a>
2010-09-01 16:26:55 +0000

    增加SelectAllCheckBox的CheckedChanged事件

--- DataList/DataListPage.Designer.cs
+++ DataList/DataListPage.Designer.cs
@@ -50,18 +50,13 @@
             // 
             // toolChkSelectAll
             // 
-            this.toolChkSelectAll.AccessibleDescription = null;
             resources.ApplyResources(this.toolChkSelectAll, "toolChkSelectAll");
             this.toolChkSelectAll.BackColor = System.Drawing.Color.Transparent;
-            this.toolChkSelectAll.BackgroundImage = null;
             // 
             // toolChkSelectAll
             // 
-            this.toolChkSelectAll.CheckBox.AccessibleDescription = null;
             resources.ApplyResources(this.toolChkSelectAll.CheckBox, "toolChkSelectAll");
             this.toolChkSelectAll.CheckBox.BackColor = System.Drawing.Color.Transparent;
-            this.toolChkSelectAll.CheckBox.BackgroundImage = null;
-            this.toolChkSelectAll.CheckBox.Font = null;
             this.toolChkSelectAll.CheckBox.Name = "toolStripCheckedBox1";
             this.toolChkSelectAll.CheckBox.UseVisualStyleBackColor = false;
             this.toolChkSelectAll.Name = "toolChkSelectAll";
@@ -69,11 +64,7 @@
             // 
             // toolStrip1
             // 
-            this.toolStrip1.AccessibleDescription = null;
-            this.toolStrip1.AccessibleName = null;
             resources.ApplyResources(this.toolStrip1, "toolStrip1");
-            this.toolStrip1.BackgroundImage = null;
-            this.toolStrip1.Font = null;
             this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripSeparator3,
@@ -95,51 +86,36 @@
             // 
             // toolStripSeparator3
             // 
-            this.toolStripSeparator3.AccessibleDescription = null;
-            this.toolStripSeparator3.AccessibleName = null;
-            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
             this.toolStripSeparator3.Name = "toolStripSeparator3";
+            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
             // 
             // toolSeparatorSelAll
             // 
-            this.toolSeparatorSelAll.AccessibleDescription = null;
-            this.toolSeparatorSelAll.AccessibleName = null;
-            resources.ApplyResources(this.toolSeparatorSelAll, "toolSeparatorSelAll");
             this.toolSeparatorSelAll.Name = "toolSeparatorSelAll";
+            resources.ApplyResources(this.toolSeparatorSelAll, "toolSeparatorSelAll");
             // 
             // toolBtnFirst
             // 
-            this.toolBtnFirst.AccessibleDescription = null;
-            this.toolBtnFirst.AccessibleName = null;
-            resources.ApplyResources(this.toolBtnFirst, "toolBtnFirst");
-            this.toolBtnFirst.BackgroundImage = null;
             this.toolBtnFirst.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolBtnFirst, "toolBtnFirst");
             this.toolBtnFirst.Name = "toolBtnFirst";
             this.toolBtnFirst.Click += new System.EventHandler(this.toolBtnFirst_Click);
             // 
             // toolLblTotal
             // 
-            this.toolLblTotal.AccessibleDescription = null;
-            this.toolLblTotal.AccessibleName = null;
             this.toolLblTotal.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            resources.ApplyResources(this.toolLblTotal, "toolLblTotal");
-            this.toolLblTotal.BackgroundImage = null;
             this.toolLblTotal.Name = "toolLblTotal";
+            resources.ApplyResources(this.toolLblTotal, "toolLblTotal");
             // 
             // toolBtnPrev
             // 
-            this.toolBtnPrev.AccessibleDescription = null;
-            this.toolBtnPrev.AccessibleName = null;
-            resources.ApplyResources(this.toolBtnPrev, "toolBtnPrev");
-            this.toolBtnPrev.BackgroundImage = null;
             this.toolBtnPrev.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolBtnPrev, "toolBtnPrev");
             this.toolBtnPrev.Name = "toolBtnPrev";
             this.toolBtnPrev.Click += new System.EventHandler(this.toolBtnPrev_Click);
             // 
             // toolTxtIndex
             // 
-            this.toolTxtIndex.AccessibleDescription = null;
-            this.toolTxtIndex.AccessibleName = null;
             resources.ApplyResources(this.toolTxtIndex, "toolTxtIndex");
             this.toolTxtIndex.Name = "toolTxtIndex";
             this.toolTxtIndex.Validated += new System.EventHandler(this.toolTxtIndex_Validated);
@@ -147,57 +123,40 @@
             // 
             // toolLblPage
             // 
-            this.toolLblPage.AccessibleDescription = null;
-            this.toolLblPage.AccessibleName = null;
-            resources.ApplyResources(this.toolLblPage, "toolLblPage");
-            this.toolLblPage.BackgroundImage = null;
             this.toolLblPage.Name = "toolLblPage";
+            resources.ApplyResources(this.toolLblPage, "toolLblPage");
             // 
             // toolBtnNext
             // 
-            this.toolBtnNext.AccessibleDescription = null;
-            this.toolBtnNext.AccessibleName = null;
-            resources.ApplyResources(this.toolBtnNext, "toolBtnNext");
-            this.toolBtnNext.BackgroundImage = null;
             this.toolBtnNext.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolBtnNext, "toolBtnNext");
             this.toolBtnNext.Name = "toolBtnNext";
             this.toolBtnNext.Click += new System.EventHandler(this.toolBtnNext_Click);
             // 
             // toolBtnLast
             // 
-            this.toolBtnLast.AccessibleDescription = null;
-            this.toolBtnLast.AccessibleName = null;
-            resources.ApplyResources(this.toolBtnLast, "toolBtnLast");
-            this.toolBtnLast.BackgroundImage = null;
             this.toolBtnLast.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.toolBtnLast, "toolBtnLast");
             this.toolBtnLast.Name = "toolBtnLast";
             this.toolBtnLast.Click += new System.EventHandler(this.toolBtnLast_Click);
             // 
             // toolStripSeparator1
             // 
-            this.toolStripSeparator1.AccessibleDescription = null;
-            this.toolStripSeparator1.AccessibleName = null;
-            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
             this.toolStripSeparator1.Name = "toolStripSeparator1";
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
             // 
             // toolStripLabel1
             // 
-            this.toolStripLabel1.AccessibleDescription = null;
-            this.toolStripLabel1.AccessibleName = null;
-            resources.ApplyResources(this.toolStripLabel1, "toolStripLabel1");
-            this.toolStripLabel1.BackgroundImage = null;
             this.toolStripLabel1.Name = "toolStripLabel1";
+            resources.ApplyResources(this.toolStripLabel1, "toolStripLabel1");
             // 
             // toolCboPageSize
             // 
-            this.toolCboPageSize.AccessibleDescription = null;
-            this.toolCboPageSize.AccessibleName = null;
             this.toolCboPageSize.AutoCompleteCustomSource.AddRange(new string[] {
             resources.GetString("toolCboPageSize.AutoCompleteCustomSource"),
             resources.GetString("toolCboPageSize.AutoCompleteCustomSource1"),
             resources.GetString("toolCboPageSize.AutoCompleteCustomSource2"),
             resources.GetString("toolCboPageSize.AutoCompleteCustomSource3")});
-            resources.ApplyResources(this.toolCboPageSize, "toolCboPageSize");
             this.toolCboPageSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.toolCboPageSize.DropDownWidth = 50;
             this.toolCboPageSize.Items.AddRange(new object[] {
@@ -206,33 +165,25 @@
             resources.GetString("toolCboPageSize.Items2"),
             resources.GetString("toolCboPageSize.Items3")});
             this.toolCboPageSize.Name = "toolCboPageSize";
+            resources.ApplyResources(this.toolCboPageSize, "toolCboPageSize");
             this.toolCboPageSize.SelectedIndexChanged += new System.EventHandler(this.toolCboPageSize_SelectedIndexChanged);
             // 
             // toolStripLabel2
             // 
-            this.toolStripLabel2.AccessibleDescription = null;
-            this.toolStripLabel2.AccessibleName = null;
-            resources.ApplyResources(this.toolStripLabel2, "toolStripLabel2");
-            this.toolStripLabel2.BackgroundImage = null;
             this.toolStripLabel2.Name = "toolStripLabel2";
+            resources.ApplyResources(this.toolStripLabel2, "toolStripLabel2");
             // 
             // toolStripSeparator2
             // 
-            this.toolStripSeparator2.AccessibleDescription = null;
-            this.toolStripSeparator2.AccessibleName = null;
             this.toolStripSeparator2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
             this.toolStripSeparator2.Name = "toolStripSeparator2";
+            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
             // 
             // DataListPage
             // 
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = null;
             this.Controls.Add(this.toolStrip1);
-            this.Font = null;
             this.Name = "DataListPage";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
