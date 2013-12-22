7b04d864c8344f8769bcc30653716c5a723be7eb
vinsonhwj <vinsonhwj@gmail.com>
2013-12-22 16:40:33 +0000

    xNumericUpDown增加SetValueToControl

--- CommonControls/xNumericUpDown.cs
+++ CommonControls/xNumericUpDown.cs
@@ -1,29 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Forms;
+﻿using hwj.UserControls.Interface;
+using System;
 using System.ComponentModel;
-using hwj.UserControls.Interface;
+using System.Windows.Forms;
 
 namespace hwj.UserControls.CommonControls
 {
     public class xNumericUpDown : NumericUpDown, IEnterEqualTab, IValueChanged
     {
         #region Property
+
         [DefaultValue(true)]
         public bool EnterEqualTab { get; set; }
+
         /// <summary>
         /// 设置引发hwj.UserControls.ValueChanged事件的对象
         /// </summary>
         [DefaultValue(null), Description("设置引发hwj.UserControls.ValueChanged事件的对象"), Browsable(false)]
         protected Function.Verify.ValueChangedHandle ValueChangedHandle { get; set; }
-        #endregion
+
+        [Description("当值改变时,同时赋值给指定的控件")]
+        public xNumericUpDown SetValueToControl { get; set; }
+
+        #endregion Property
 
         public xNumericUpDown()
         {
             EnterEqualTab = true;
             ValueChangedHandle = Common.ValueChanged;
             ValueChangedEnabled = true;
+            SetValueToControl = null;
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
@@ -37,6 +42,10 @@ namespace hwj.UserControls.CommonControls
         {
             if (ValueChangedEnabled && this.Focused && ValueChangedHandle != null)
                 ValueChangedHandle.IsChanged = true;
+            if (SetValueToControl != null)
+            {
+                SetValueToControl.Value = this.Value;
+            }
             base.OnValueChanged(e);
         }
 
@@ -48,6 +57,6 @@ namespace hwj.UserControls.CommonControls
         [DefaultValue(true), Description("获取或设置ValueChanged事件的IsChanged属性")]
         public bool ValueChangedEnabled { get; set; }
 
-        #endregion
+        #endregion IValueChanged Members
     }
 }
